a1fefaf6b223762ef9dfffef5d290670e0fa5d76
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-05-19 07:04:38 +0100

    Updating Nav-list

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,40 @@
 const menuToggle = document.querySelector(".menu-toggle");
 
-const navigation = document.querySelector("nav");
+const navContainer = document.querySelector("nav");
+const navigation = document.querySelector(".nav-list");
+const navItems = document.querySelectorAll(".nav-item");
+
+const contact = document.querySelector(".contact");
+const contactToggle = document.querySelector("#contactToggle");
 const header = document.querySelector("header");
 
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", (e) => {
+    e.preventDefault;
+    menuToggleOnActive();
+  });
+});
+
 menuToggle.addEventListener("click", menuToggleOnActive);
+contactToggle.addEventListener("click", showContact);
 
 function menuToggleOnActive() {
   menuToggle.classList.toggle("active");
+  navContainer.classList.toggle("active");
   navigation.classList.toggle("active");
-  
 
-  console.log(menuToggleClose);
+  contactToggle.classList.remove("active");
+  contact.classList.remove("active");
 }
 
 function menuToggleOnClose() {
   navigation.classList.remove("active");
 }
+
+// function showContact() {
+//   navContainer.classList.toggle("active");
+//   contactToggle.classList.toggle("active");
+//   contact.classList.toggle("active");
+
+//   // navigation.classList.remove('active')
+// }
