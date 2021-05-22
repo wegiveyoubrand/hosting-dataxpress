c23b696709adba9a272c8189359c2ca2a66558c0
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-05-22 11:05:08 +0100

    Update Slides with JS

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,116 +1,28 @@
-const slides = Array.from(document.querySelectorAll(".slide"));
+const slides = document.querySelectorAll(".slide");
+let i = 0;
 
-const slidesContainer = document.querySelector(".slides");
+setInterval(function () {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("show");
+    slide.classList.add("hide");
+  });
+  if (i == 0) {
+    slides[i].classList.add("show");
+    slides[i].classList.remove("hide");
+  } else if (i == slides.length) {
+    slides[i - 1].classList.add("hide");
+    slides[i - 1].classList.remove("show");
 
-const dotsEl = document.querySelector(".dots");
-let timeOutId;
-
-function getNextPrev() {
-  const activeSlide = document.querySelector(".slide.active");
-  const activeIndex = slides.indexOf(activeSlide);
-  let prev, next;
-  if (activeIndex === slides.length - 1) {
-    next = slides[0];
+    slides[0].classList.add("show");
+    slides[0].classList.remove("hide");
+    i = 0;
   } else {
-    next = slides[activeIndex + 1];
+    slides[i - 1].classList.add("hide");
+    slides[i - 1].classList.remove("show");
+
+    slides[i].classList.add("show");
+    slides[i].classList.remove("hide");
   }
 
-  if (activeIndex === 0) {
-    prev = slides[slides.length - 1];
-  } else {
-    prev = slides[activeIndex - 1];
-  }
-  return [next, prev];
-}
-
-function getPosition() {
-  const activeSlide = document.querySelector(".slide.active");
-  const activeIndex = slides.indexOf(activeSlide);
-  const [next, prev] = getNextPrev();
-  slides.forEach((slide, index) => {
-    if (index === activeIndex) {
-      slide.style.transform = "translateX(0)";
-    } else if (slide === prev) {
-      slide.style.transform = "translateX(-100%)";
-    } else {
-      slide.style.transform = "translateX(100%)";
-    }
-    slide.addEventListener("transitionend", () => {
-      slide.classList.remove("top");
-    });
-  });
-}
-function getNextSlide() {
-  clearInterval(timeOutId);
-  const current = document.querySelector(".slide.active");
-  const [next, prev] = getNextPrev();
-  if (current.classList.contains("top")) {
-    return;
-  }
-  current.classList.add("top");
-  next.classList.add("top");
-  current.style.transform = "translate(-100%)";
-  current.classList.remove("active");
-  next.style.transform = "translateX(0)";
-  next.classList.add("active");
-  getPosition();
-  getActiveDot();
-  autoLoop();
-}
-function getPrevSlide() {
-  clearInterval(timeOutId);
-  const current = document.querySelector(".active");
-  const [next, prev] = getNextPrev();
-  current.classList.add("top");
-  next.classList.add("top");
-  current.style.transform = "translate(100%)";
-  current.classList.remove("active");
-  next.style.transform = "translateX(0)";
-  next.classList.add("active");
-  getPosition();
-  getActiveDot();
-  autoLoop();
-}
-// Dots
-slides.forEach((slide) => {
-  const dot = document.createElement("div");
-  dot.classList.add("dot");
-  dotsEl.appendChild(dot);
-});
-function getActiveDot() {
-  const allDots = document.querySelectorAll(".dots .dot");
-  allDots.forEach((dot) => {
-    dot.classList.remove("active");
-  });
-  const activeSlide = document.querySelector(".slide.active");
-  const activeIndex = slides.indexOf(activeSlide);
-  allDots[activeIndex].classList.add("active");
-
-}
-function functionalDots() {
-  const allDots = document.querySelectorAll(".dots .dot");
-  allDots.forEach((dot, index) => {
-    dot.addEventListener("click", () => {
-      getDotSlide(index);
-    });
-  });
-}
-function getDotSlide(index) {
-  clearTimeout(timeOutId);
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
-  const current = slides[index];
-  current.classList.add("active");
-  getPosition();
-  getActiveDot();
-  autoLoop();
-}
-function autoLoop() {
-  timeOutId = setTimeout(() => {
-    getNextSlide();
-  }, 500);
-}
-getActiveDot();
-functionalDots();
-autoLoop();
+  i++;
+}, 5000);
